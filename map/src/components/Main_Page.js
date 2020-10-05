57b0edc669ad314df2d7dad468fec8047f6580fe
Abel Barrera Duran <aduran@Aduran-Macbook.local>
2020-10-04 22:38:12 -0400

    row col formatting

--- map/src/components/Main_Page.js
+++ map/src/components/Main_Page.js
@@ -1,12 +1,27 @@
 import React from "react";
 import * as d3 from "d3";
+import streets from "../data_files/streets.json";
+import deathAttribs from "../data_files/deathdays.csv";
+import deathDays from "../data_files/deaths_age_sex.csv";
+import { Row, Col } from "react-bootstrap";
 
-let streetsJson = require("../data_files/streets.json");
-console.log(streetsJson);
+//console.log(streets);
 
 const Thing = (
   <div>
-    <h1> Hello World </h1>
+    <Row>
+      <Col sm={8}>
+        <h1> Hello World </h1>
+      </Col>
+      <Col sm={4}>
+        <Row>
+          <h1> Hello World </h1>
+        </Row>
+        <Row>
+          <h1> Hello World </h1>
+        </Row>
+      </Col>
+    </Row>
   </div>
 );
 
