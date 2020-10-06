ce23ba0861cf8ad890610b7fbd3437dd3d6070c5
Abel Barrera Duran <aduran@Aduran-Macbook.local>
2020-10-06 18:58:22 -0400

    Updates

---
+++ precache-manifest.f76e1cdad000926d65bbc6d2b429a0d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebf23bc1d9e11d41779ed9dacdaf678",
+    "url": "/data-viz-project-1/index.html"
+  },
+  {
+    "revision": "4023177a0cd7dc3eb643",
+    "url": "/data-viz-project-1/static/css/main.5a11c72a.chunk.css"
+  },
+  {
+    "revision": "5a64295e8aa4d609b97c",
+    "url": "/data-viz-project-1/static/js/2.bb28dc81.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/data-viz-project-1/static/js/2.bb28dc81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4023177a0cd7dc3eb643",
+    "url": "/data-viz-project-1/static/js/main.bf3cafa0.chunk.js"
+  },
+  {
+    "revision": "87acf1fb8023c124ff57",
+    "url": "/data-viz-project-1/static/js/runtime-main.945ca11a.js"
+  },
+  {
+    "revision": "a61793db8f249bb3c8a4f113e3714775",
+    "url": "/data-viz-project-1/static/media/IUPUI_Logo_49.a61793db.jpg"
+  },
+  {
+    "revision": "91268c88c6e6906d993a12b4a6ecf0a1",
+    "url": "/data-viz-project-1/static/media/deathdays.91268c88.csv"
+  },
+  {
+    "revision": "34591a292ad5c9bc0e068653b3c18523",
+    "url": "/data-viz-project-1/static/media/deaths_age_sex.34591a29.csv"
+  },
+  {
+    "revision": "729cd622993bf239ab344bde34ea485f",
+    "url": "/data-viz-project-1/static/media/pumps.729cd622.csv"
+  }
+]);
