24fa6bae92c25441893edaa854a9c91b6e8e114b
Abel Barrera Duran <aduran@Aduran-Macbook.local>
2020-10-05 21:38:59 -0400

    graph still flipped idk why

--- map/src/components/Main_Page.js
+++ map/src/components/Main_Page.js
@@ -5,6 +5,7 @@ import deathDays from "../data_files/deaths_age_sex.csv";
 import pumpsLoc from "../data_files/pumps.csv";
 import dropSvg from "../assets/droplet.svg";
 import { Row, Col, Container } from "react-bootstrap";
+import rotateY from "react-p5";
 
 const DrawStreets = () => {
   const streetsJson = require("../data_files/streets.json");
@@ -14,12 +15,15 @@ const DrawStreets = () => {
     const map = d3
       .select(ref.current)
       .attr("width", "100%")
-      .attr("height", "800px")
+      .attr("height", "800%")
       .call(
         d3.behavior.zoom().on("zoom", function () {
+          var me = map.node();
+          var x1 = me.getBBox().x + me.getBBox().width / 2;
+          var y1 = me.getBBox().y + me.getBBox().height / 2;
           map.attr(
             "transform",
-            `translate(${d3.event.translate}) scale(${d3.event.scale})`
+            `translate(${d3.event.translate}) scale(${d3.event.scale}) rotate(180, ${x1}, ${y1})`
           );
         })
       )
@@ -54,6 +58,7 @@ const DrawStreets = () => {
             .y((d) => offset(d.y))
         );
 
+      //draw pumps on map
       d3.csv(pumpsLoc, function (data) {
         pumps
           .selectAll("circle")
@@ -64,27 +69,139 @@ const DrawStreets = () => {
           .attr("cy", (d) => offset(d.y))
           .attr("class", "pump");
       });
+
+      // Work House
+      map
+        .append("g")
+        .append("rect")
+        .attr("class", "workHouse")
+        .attr("x", 523)
+        .attr("y", 195)
+        .attr("width", 60)
+        .attr("height", 50)
+        .style("opacity", 0.1);
+
+      // Work House label
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "workHouseLabel")
+        .attr("x", 518)
+        .attr("y", -243)
+        .text("Work House");
+
+      // Brewery
+      map
+        .append("g")
+        .append("rect")
+        .attr("class", "brewery")
+        .attr("x", 610)
+        .attr("y", 76)
+        .attr("width", 28)
+        .attr("height", 48)
+        .style("opacity", 0.1);
+
+      // Brewery Label
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "breweryLabel")
+        .attr("x", -130)
+        .attr("y", -620)
+        .text("Brewery");
+
+      // Golden Square
+      map
+        .append("g")
+        .append("rect")
+        .attr("class", "goldenSquare")
+        .attr("x", 439)
+        .attr("y", -62)
+        .attr("width", 50)
+        .attr("height", 50)
+        .style("opacity", 0.1);
+
+      // Golden Square Label pt 1
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "goldenSquareLabel")
+        .attr("x", 445)
+        .attr("y", 30)
+        .text("Golden");
+
+      // Golden Square Label pt 1
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "goldenSquareLabel")
+        .attr("x", 445)
+        .attr("y", 40)
+        .text("Square");
+
+      // Broad street
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "broadStreet")
+        .attr("x", 590)
+        .attr("y", -148)
+        .text("Broad Street");
+
+      // Great Marlborough Street
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "greatStreet")
+        .attr("x", 385)
+        .attr("y", -336)
+        .text("Great Marlborough Street");
+
+      // Regent Street
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "regentStreet")
+        .attr("x", -215)
+        .attr("y", -358)
+        .text("Regent Street");
+
+      // Brewer Street
+      map
+        .append("g")
+        .append("text")
+        .attr("class", "brewerStreet")
+        .attr("x", 424)
+        .attr("y", 160)
+        .text("Brewer Street");
     };
 
+    //MakeMap();
     //init map
-    const loadMakeMap = new Promise((resolve, reject) => {
+    const loadMakeMap = new Promise(function (resolve) {
       MakeMap();
+      resolve(1);
     });
 
     //rotate map
-    loadMakeMap.then((upd) => {
+    loadMakeMap.then(() => {
       map.attr("transform", function () {
         var me = map.node();
-        var x1 = me.getBBox().x + me.getBBox().width / 2; //the center x about which you want to rotate
-        var y1 = me.getBBox().y + me.getBBox().height / 2; //the center y about which you want to rotate
-
-        return `rotate(180, ${x1}, ${y1})`; //rotate 180 degrees about x and y
+        var x1 = me.getBBox().x + me.getBBox().width / 2;
+        var y1 = me.getBBox().y + me.getBBox().height / 2;
+        //return `scale(1, -1)`;
+        return `rotate(180, ${x1}, ${y1})`;
       });
     });
+    //   .then(() => {
+    //     map.attr("transform", function (d) {
+    //       return this.getAttribute("transform") + " rotateY(180)";
+    //     });
+    //   });
 
     // svgElement.append("circle").attr("cx", 200).attr("cy", 70).attr("r", 50);
   }, []);
-
+  //
   return <svg ref={ref}></svg>;
 };
 
