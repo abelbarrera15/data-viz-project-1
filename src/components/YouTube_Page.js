ccaa38dda55fb332fae95a505c64972a44a40fdf
Abel Barrera Duran <aduran@Aduran-Macbook.lan>
2020-11-24 22:55:16 -0600

    push up video

--- src/components/YouTube_Page.js
+++ src/components/YouTube_Page.js
@@ -6,7 +6,7 @@ const YouTubePage = (
     <h6 style={{ paddingLeft: "0px", fontWeight: "bold" }}>
       Youtube Based Demonstration
     </h6>
-    <ReactPlayer url="https://youtu.be/YEjvn0ZYF0I" />
+    <ReactPlayer url="https://youtu.be/55UNyftq6JU" />
   </div>
 );
 
